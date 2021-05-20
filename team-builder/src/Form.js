c5aaf72cc5d426ca404d51c9fab016755e02dbba
everybodyneedsis <jacobkylefranklin@gmail.com>
2021-05-19 20:30:16 -0400

    made the role a dropbox also added in a submit button, cant get my unput field to actually let me write text in them

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -7,28 +7,45 @@ export default function Form(props) {
         submit,
     } = props
 
+    const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+    }
+
     const onChange = evt => {
         const { name, value } = evt.target
-      }
+    }
 
-      return (
-          <form>
-            <label>Member Name:
-                <input 
-                    value={values.memberName}
-                    onChange={onChange}
-                    type='text'
-                    name='memberName'
-                />
-            </label>
-            <label>Email:
-                <input
-                    value={values.email}
-                    onChange={onChange}
-                    name='email'
-                    type='email'
-                />
+    return (
+        <form onSubmit={onSubmit}>
+        <label>Member Name:
+             <input 
+                value={values.memberName}
+                onChange={onChange}
+                type='text'
+                name='memberName'
+            />
         </label>
-          </form>
-      )
-}
+        <label>Email:
+            <input
+                value={values.email}
+                onChange={onChange}
+                name='email'
+                type='email'
+            />
+        </label>
+        <label>Role
+          <select 
+            value={values.role} 
+            name='role' 
+            onChange={onChange}>
+                <option value=''>-- Select a Role --</option>
+                <option value='SD'>Software Developer</option>
+                <option value='3D'>3D Animator</option>
+                <option value='Instructor'>CopyWriter</option>
+          </select>
+        </label>
+
+        <button type='submit'>Submit</button>
+    </form>
+)}
