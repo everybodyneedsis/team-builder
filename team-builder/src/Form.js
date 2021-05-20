312c1baae5ec6ef9da4e3dea6c506fd846d766e2
everybodyneedsis <jacobkylefranklin@gmail.com>
2021-05-19 21:28:37 -0400

    adding some styles

--- team-builder/src/Form.js
+++ team-builder/src/Form.js
@@ -18,34 +18,34 @@ export default function Form(props) {
 
     return (
         <form onSubmit={onSubmit}>
-        <label>Member Name:
-             <input 
-                value={values.memberName}
-                onChange={onChange}
-                type='text'
-                name='memberName'
-            />
-        </label>
-        <label>Email:
-            <input
-                value={values.email}
-                onChange={onChange}
-                name='email'
-                type='email'
-            />
-        </label>
-        <label>Role
-          <select 
-            value={values.role} 
-            name='role' 
-            onChange={onChange}>
-                <option value=''>-- Select a Role --</option>
-                <option value='SD'>Software Developer</option>
-                <option value='3D'>3D Animator</option>
-                <option value='Instructor'>CopyWriter</option>
-          </select>
-        </label>
+            <label>Member Name:
+                <input 
+                    value={values.memberName}
+                    onChange={onChange}
+                    type='text'
+                    name='memberName'
+                />
+            </label>
+            <label>Email:
+                <input
+                    value={values.email}
+                    onChange={onChange}
+                    name='email'
+                    type='email'
+                />
+            </label>
+            <label>Role
+            <select 
+                value={values.role} 
+                name='role' 
+                onChange={onChange}>
+                    <option value=''>-- Select a Role --</option>
+                    <option value='SD'>Software Developer</option>
+                    <option value='3D'>3D Animator</option>
+                    <option value='Instructor'>CopyWriter</option>
+            </select>
+            </label>
 
-        <button type='submit'>Submit</button>
+            <button type='submit'>Submit</button>
     </form>
 )}
